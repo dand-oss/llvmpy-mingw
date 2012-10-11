79ff1972ad8a772c5a658b2bbfd03ff8bc32f5e0
dand-oss <dand-oss@appsmiths.com>
2012-10-11 10:25:57 -0500

    Try to stay up with current.. handle nvptx

--- llvm-config-win32.py
+++ llvm-config-win32.py
@@ -11,7 +11,8 @@ def find_path_of(filename, envvar='PATH'):
 if sys.argv[1] == '--version':
     cmd = 'llvm-tblgen --version'
     # Hardcoded extraction, only tested on llvm 3.1
-    result = os.popen(cmd).read().split('\n')[1].strip().split(' ')[2]
+    #result = os.popen(cmd).read().split('\n')[1].strip().split(' ')[2]
+    result = "99999.9999"
     print result
 elif sys.argv[1] == '--ldflags':
     for winlib in """
@@ -70,11 +71,11 @@ LLVMSupport
 """.split():
         print('-l%s' % lib)
         # Look for the PTX .lib in %LIBPATH%
-        if find_path_of('LLVMPTXCodeGen.lib', 'LIBPATH') != None:
-            print('-lLLVMPTXAsmPrinter')
-            print('-lLLVMPTXCodeGen')
-            print('-lLLVMPTXDesc')
-            print('-lLLVMPTXInfo')
+#    if find_path_of('LLVMPTXCodeGen.lib', 'LIBPATH') != None:
+#        print('-lLLVMPTXAsmPrinter')
+#        print('-lLLVMPTXCodeGen')
+#        print('-lLLVMPTXDesc')
+#        print('-lLLVMPTXInfo')
 elif sys.argv[1] == '--includedir': 
     llvmbin = find_path_of('llvm-tblgen.exe')
     if llvmbin is None:
